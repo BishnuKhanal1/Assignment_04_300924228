876e8660af44a55e4814721c6a7720a2a2540142
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-14 20:23:48 -0400

    Initial commit / Program class created

--- Assignment_04_300924228/Program.cs
+++ Assignment_04_300924228/Program.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*Name: Bishnu Khanal
+ * Date: July 13, 2017
+ * Description: This is the SuperHuman class
+ * Version:0.4 - Added Override(ToString) method and Displayskills Method to SuperHuman class
+ * GitHub Link: https://github.com/BishnuKhanal1/COMP123-s2017-Lesson9B-Lab24/commits/master   
+ */
 namespace Assignment_04_300924228
 {
     class Program
