859f21ab4ac620acc8b826e67348ed19c29ad668
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-14 20:29:46 -0400

    Created Abstract Planet class

---
+++ Assignment_04_300924228/Planet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Name: Bishnu Khanal
+ * Date: July 14, 2017
+ * Description: This is the abstract Planet class
+ * Version:0.1 - Created  the abstract Planet class
+ * GitHub Link: https://github.com/BishnuKhanal1/COMP123-s2017-Lesson9B-Lab24/commits/master   
+ */
+namespace Assignment_04_300924228
+{
+    public abstract class Planet
+    {
+    }
+}
--- Assignment_04_300924228/Program.cs
+++ Assignment_04_300924228/Program.cs
@@ -4,15 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*Name: Bishnu Khanal
- * Date: July 13, 2017
- * Description: This is the SuperHuman class
- * Version:0.4 - Added Override(ToString) method and Displayskills Method to SuperHuman class
+ * Date: July 14, 2017
+ * Description: This is the Program class
+ * Version:0.1 - Created  the program class
  * GitHub Link: https://github.com/BishnuKhanal1/COMP123-s2017-Lesson9B-Lab24/commits/master   
  */
 namespace Assignment_04_300924228
 {
     class Program
     {
+        /// <summary>
+        /// This is the program class
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
         }
