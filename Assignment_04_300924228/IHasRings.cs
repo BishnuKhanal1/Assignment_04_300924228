23620472be3505db00d4d527d882a83fb1641628
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-20 22:43:11 -0400

    Created IHasRings Inteface which implements bool HasRings method

--- Assignment_04_300924228/IHasRings.cs
+++ Assignment_04_300924228/IHasRings.cs
@@ -4,17 +4,17 @@ using System.Linq;
 using System.Text;
 /*Name: Bishnu Khanal
  * Date: July 20, 2017
- * Description: This is the IHasMoons interface
- * Version:0.2 - Created IHasMoon Inteface which implements bool HasMoon() method
+ * Description: This is the IHasRings interface
+ * Version:0.1 - Created IHasRings Inteface which implements bool HasRings method
  * GitHub Link: https://github.com/BishnuKhanal1/COMP123-s2017-Lesson9B-Lab24/commits/master   
  */
 namespace Assignment_04_300924228
 {
     /// <summary>
-    /// This is IHasMoons Interface
+    /// This is IHasRings interface
     /// </summary>
-    public interface IHasMoons
+    public interface IHasRings
     {
-        bool HasMoons(); 
+        bool HasRings();
     }
 }
