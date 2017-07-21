8366fc9a8efd92014050bc07e17300c608a361a7
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-20 22:46:00 -0400

    Created IHabitable Inteface which implements bool Habitable() method

---
+++ Assignment_04_300924228/IHabitable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Name: Bishnu Khanal
+ * Date: July 20, 2017
+ * Description: This is the IHabitable interface
+ * Version:0.1 - Created IHabitable Inteface which implements bool Habitable() method
+ * GitHub Link: https://github.com/BishnuKhanal1/COMP123-s2017-Lesson9B-Lab24/commits/master   
+ */
+namespace Assignment_04_300924228
+{
+    /// <summary>
+    /// This is IHabotable interface
+    /// </summary>
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
