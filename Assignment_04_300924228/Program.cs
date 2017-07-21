bf43c8d1ca0fc1a15eab0b7f2b35f28183d67fd8
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-21 12:00:21 -0400

    Implemented WaitForAnyKey() method in the driver class

--- Assignment_04_300924228/Program.cs
+++ Assignment_04_300924228/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 /*Name: Bishnu Khanal
  * Date: July 14, 2017
  * Description: This is the Program class
- * Version:0.2 - Implemented (tested) GiantPlanet object and terrestorialPlanet object
+ * Version:0.3 - Implemented WaitForAnyKey() method in the driver class
  * GitHub Link: https://github.com/BishnuKhanal1/COMP123-s2017-Lesson9B-Lab24/commits/master   
  */
 namespace Assignment_04_300924228
@@ -23,6 +23,20 @@ namespace Assignment_04_300924228
             Console.WriteLine(giantPlanet.ToString());
             TerrestrialPlanet terrestrialPlannet = new TerrestrialPlanet("Earth", 50724, 8.681025, true);
             Console.WriteLine(terrestrialPlannet.ToString());
+
+            //Invoke WaitForAnyKey() method
+            WaitForAnyKey();
+        }
+        /// <summary>
+        /// This is WaitForAnyKey method which does not retur anything
+        /// Prompts the message in console to enter any key to exit
+        /// </summary>
+        public static void WaitForAnyKey()
+        {
+            string message;
+            message = "Please enter any key to exit";
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
