c1df0aa256a9df6c6ba068eea05e4e3b3e745254
Bishnu Khanal <bkhanal1@my.centennialcollege.ca>
2017-07-20 22:38:38 -0400

    Created IHasMoon Interface

---
+++ Assignment_04_300924228/IHasMoons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/*Name: Bishnu Khanal
+ * Date: July 20, 2017
+ * Description: This is the IHasMoons interface
+ * Version:0.1 - Created IHasMoon Inteface
+ * GitHub Link: https://github.com/BishnuKhanal1/COMP123-s2017-Lesson9B-Lab24/commits/master   
+ */
+namespace Assignment_04_300924228
+{
+    /// <summary>
+    /// This is IHasMoons Interface
+    /// </summary>
+    public interface IHasMoons
+    {
+        bool HasMoons(); 
+    }
+}
